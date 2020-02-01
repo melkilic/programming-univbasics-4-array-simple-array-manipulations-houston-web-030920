8e77cecd9b07f522cc7f26262dc996c8eb3dbedc
Melike KILIC <melike180400@gmail.com>
2020-02-01 21:53:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,9 +1,9 @@
 def using_push(array, string)
-array.push(string)
+ array.push(string)
 end
 
 def using_unshift(array, string)
-array.unshift(string)
+ array.unshift(string)
 end
 
 def using_pop(array)
@@ -11,17 +11,24 @@ array.pop
 end
 
 def using_shift(array)
-array.shift
+ array.shift
 end
 
 def pop_with_args(array)
-p= array.pop(2)
+ p= array.pop(2)
 end
 
 def shift_with_args(array)
-p= array.shift(2)
+ p= array.shift(2)
 end
 
 def using_concat(chocolate, donut)
-  chocolate.concat(donut)
+ chocolate.concat(donut)
+end
+ def using_insert(array,new_element)
+  array.insert[4]= new_element
+ end
+
+def using_uniq(array)
+  array.uniq
 end
